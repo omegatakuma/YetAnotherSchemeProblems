972e09bce17bd641ce55324ed96ebaa945200cae
omegatakuma <tooth0819@gmail.com>
2012-06-01 22:09:04 +0900

    add

---
+++ max-list.scm
@@ -0,0 +1,8 @@
+(define (max-list lst)
+  (let loop ((lst (cdr lst))(n (car lst)))
+	(cond
+	  ((null? lst) n)
+	  ((> (car lst) n)
+	   (loop (cdr lst) (car lst)))
+	  (else
+		(loop (cdr lst) n)))))
